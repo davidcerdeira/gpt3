2a70766dbbef2a88bf7f30b92df596981acdcfc6
David Cerdeira <59337237+realhackerdavid@users.noreply.github.com>
2023-01-06 00:38:26 +0000

    Changed favicon and website title. Changed the colors a bit.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
         <div className='gpt3__footer-links_logo'>
           <img src={gpt3Logo} alt='gpt3_logo' />
           <p>
-            Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+            Lisbon, Portugal <br /> All Rights Reserved
           </p>
         </div>
         <div className='gpt3__footer-links_div'>
@@ -44,7 +44,7 @@ const Footer = () => {
       </div>
 
       <div className='gpt3__footer-copyright'>
-        <p>@2023 GPT-3. All rights reserved.</p>
+        <p>@2023 GPT-3. All rights reserved. Coded by David.</p>
       </div>
     </div>
   );
