158af600738a82bcd1919e4edb05a7397f9892f3
David Cerdeira <59337237+realhackerdavid@users.noreply.github.com>
2023-01-10 17:31:08 +0000

    Updated some content.

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,13 +9,13 @@ const Header = () => {
       <div className='gpt3__header section__padding' id='home'>
         <div className='gpt3__header-content'>
           <h1 className='gradient__text'>
-            Let&apos;s Build Something amazing with GPT-3 OpenAI
+            It&apos;s time. Let&apos;s Dive Into the Future with GPT-3 OpenAI
           </h1>
           <p>
-            Yet bed any for travelling assistance indulgence unpleasing. Not
-            thoughts all exercise blessing. Indulgence way everything joy
-            alteration boisterous the attachment. Party we years to order allow
-            asked of.
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+          doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+          veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          
           </p>
 
           <div className='gpt3__header-content__input'>
